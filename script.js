fba71cbc1076ecb11da424b6fe2c3d1d21cb57fb
emetdas <emet@b2bleadstaff.com>
2020-10-11 08:14:00 +0600

    portfolio-03
    nice project

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#fil").mixItUp();
+// mixItUp end
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
